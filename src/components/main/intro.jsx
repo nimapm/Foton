e3a7452b8b7f5003745e87b5e1de42ea8d497419
Nima Prmi <nima.gt10@gmail.com>
2020-07-04 06:11:22 -0700

    v0.3

--- src/components/main/intro.jsx
+++ src/components/main/intro.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
-import style from "../../assets/css/main.scss";
+
+import style from "../../assets/css/_main.scss";
 
 class Intro extends Component {
-  state = {};
+  state = {
+    title: "Untapped Potential",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore aperiam asperiores natus eius pariatur ",
+    buttons: {
+      leftBtn: ["Read More", "#"],
+      rightBtn: ["Contact us", "#"],
+    },
+  };
   render() {
+    const { title, description, buttons } = this.state;
     return (
       <React.Fragment>
-        <div>Hello</div>
+        <div className="main-container">
+          <div className="main-container-bg"></div>
+          <div className="main-container-content">
+            <div className="cnt1">
+              <div className="cnt2">
+                <div className="main-container-box">
+                  <div className="main-container-box-title">
+                    <h1>{title}</h1>
+                  </div>
+                  <div className="main-container-box-description">
+                    <p>{description}</p>
+                  </div>
+                  <div className="main-container-box-buttons">
+                    <a href={buttons.leftBtn[1]}>{buttons.leftBtn[0]}</a>
+                    <a href={buttons.rightBtn[1]}>{buttons.rightBtn[0]}</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
