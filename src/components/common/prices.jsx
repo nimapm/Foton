492111658deae0dcd0998be86c03bc954ac0d122
Nima Prmi <nima.gt10@gmail.com>
2020-07-10 11:18:24 -0700

    v5

--- src/components/common/prices.jsx
+++ src/components/common/prices.jsx
@@ -17,8 +17,31 @@ class Prices extends Component {
         ],
         link: "#",
       },
-      { price: "13", title: "", service: ["", "", "", ""], link: "#" },
-      { price: "14", title: "", service: ["", "", "", ""], link: "#" },
+      {
+        price: "37",
+        title: "Business Special Offer",
+        service: [
+          "Mobile-Optimized",
+          "Free Custom Domain",
+          "Best Hosting",
+          "Outstanding Support",
+          "Happy Customers",
+          "Happy Customers",
+        ],
+        link: "#",
+      },
+      {
+        price: "78",
+        title: "Enterprise Best Option",
+        service: [
+          "Mobile-Optimized",
+          "Free Custom Domain",
+          "Best Hosting",
+          "Outstanding Support",
+          "Happy Customers",
+        ],
+        link: "#",
+      },
     ],
   };
   render() {
@@ -29,55 +52,54 @@ class Prices extends Component {
           <div className="cnt1">
             <div className="cnt2">
               {/* */}
-
-              <div className="price-box">
-                <div className="price-box-left">
-                  <div className="price-box-left-top">
-                    <span className="price-box-left-top-span">
-                      <sup>$</sup>
-                      {prices[0].price}
-                    </span>
+              {prices.map((price, index) => (
+                <div key={"price" + index} className="price-box">
+                  <div className="price-box-left">
+                    <div className="price-box-left-top">
+                      <span className="price-box-left-top-span">
+                        <sup>$</sup>
+                        {price.price}
+                      </span>
+                    </div>
+                    <div className="price-box-left-bottom">
+                      <span>EVERY MONTH OR YEARLY</span>
+                    </div>
                   </div>
-                  <div className="price-box-left-bottom">
-                    <span>EVERY MONTH OR YEARLY</span>
-                  </div>
-                </div>
-                <div className="price-box-right">
-                  <div className="price-box-right-top">
-                    <div className="cnt1">
-                      <div className="cnt2">
-                        <div className="price-box-right-top-box">
-                          <h6>{prices[0].title}</h6>
+                  <div className="price-box-right">
+                    <div className="price-box-right-top">
+                      <div className="cnt1">
+                        <div className="cnt2 cnt-bottom">
+                          <div className="price-box-right-top-box">
+                            <h6>{price.title}</h6>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="price-box-right-middle">
+                      <div className="cnt1">
+                        <div className="cnt2 cnt-top">
+                          <div className="price-box-right-middle-box">
+                            <ul>
+                              {price.service.map((priceListItem) => (
+                                <li>{priceListItem}</li>
+                              ))}
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="price-box-right-bottom">
+                      <div className="cnt1">
+                        <div className="cnt2 cnt-left">
+                          <a href={price.link} className="read-more-btn">
+                            Read More
+                          </a>
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div className="price-box-right-middle">
-                    <div className="cnt1">
-                      <div className="cnt2">
-                        <div className="price-box-right-middle-box">
-                          <ul>
-                            <li>Mobile-Optimized</li>
-                            <li>Free Custom Domain</li>
-                            <li>Best Hosting</li>
-                            <li>Outstanding Support</li>
-                            <li>Happy Customers</li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="price-box-right-bottom">
-                    <div className="cnt1">
-                      <div className="cnt2 cnt-left">
-                        <a href="#" className="read-more-btn">
-                          Read More
-                        </a>
-                      </div>
-                    </div>
-                  </div>
                 </div>
-              </div>
+              ))}
 
               {/* */}
             </div>
